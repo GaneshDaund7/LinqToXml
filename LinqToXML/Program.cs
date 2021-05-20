88b05d3a755be78a17cb08cd40e8e80b4c5b07da
Ganesh Daund <57858907+GaneshDaund7@users.noreply.github.com>
2021-05-20 14:02:24 +0530

    Initial Commit

---
+++ LinqToXML/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LinqToXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XDocument XMLDoc = new XDocument(
+            //    new XDeclaration("1.0","UTF-8","Yes"),
+            //    new XComment("Creating an Xml Comment"),
+            //    new XElement("Students",
+            //       from Students in Student.GetAllDetails()
+            //       select new XElement("Student",new XAttribute("Id",Students.id) 
+            //                                     ,new XElement("Name",Students.name)
+            //                                     ,new XElement("Gender",Students.Gender)
+            //                                     ,new XElement("Marks",Students.Marks)
+                                                 
+                                                 
+            //                          ))
+            //    );
+
+
+           
+            //XMLDoc.Save(@"C:\Users\Lenovo\Documents\Visual Studio 2010\Projects\LinqToXML\LinqToXML\Data.xml");
+
+
+
+
+
+
+            var name=from student in XDocument.Load(@"C:\Users\Lenovo\Documents\Visual Studio 2010\Projects\LinqToXML\LinqToXML\Data.xml")
+
+                         .Descendants("Student")
+                         select student;
+
+
+                     foreach(var res in name)
+                        Console.WriteLine(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+                
+               
+        }
+    }
+}
---
+++ LinqToXML/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqToXML
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Gender { get; set; }
+        public int Marks { get; set; }
+
+
+
+        public static Student[] GetAllDetails()
+        {
+            Student[] std = new Student[4];
+            std[0] = new Student { id = 1, name = "Ganesh", Gender = "Male", Marks = 89 };
+            std[1] = new Student { id = 2, name = "Abhijeet", Gender = "Male", Marks = 90 };
+            std[2] = new Student { id = 3, name = "Rohit", Gender = "Male", Marks = 79 };
+            std[3] = new Student { id = 4, name = "GK", Gender = "Male", Marks = 99 };
+            return std;
+        }
+    }
+}
